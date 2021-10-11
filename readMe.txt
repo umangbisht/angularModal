4a201ef512c7947eee2a6c9f8b0fac2d9a919c01
Rahul Gupta <rahul.gupta@owebest.in>
2021-10-11 11:22:09 +0530

    angular initial commit

---
+++ readMe.txt
@@ -0,0 +1,4 @@
+1. extract the files and change directory to angular project environment using cd command
+2. run command into thje project environment by command:- npm install
+3. RUn the project with command:- npm start
+4: hit the url :- https://localhost:4200 on the browser.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { ModalModule } from 'ng-modal-lib';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ModalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
